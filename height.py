b0240a5b516c2d1098493fcbe14af6be0a0c8696
Julian Fuchs <jfuchs@mit.edu>
2016-02-29 00:20:22 -0500

    Actually add height.py this time

---
+++ height.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import requests
+import sys
+import json
+
+if len(sys.argv) != 2:
+    print("Usage: {} <block ID>".format(sys.argv[0]))
+    sys.exit(1)
+
+i = sys.argv[1]
+h = 0
+
+while True:
+    print(i)
+    r = requests.get("http://6857coin.csail.mit.edu:8080/block/{}".format(i))
+    try:
+        b = json.loads(r.text)
+        h += 1
+        i = b["header"]["parentid"]
+    except:
+        break
+
+print("Height: {}".format(h))
